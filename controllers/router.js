3a1c4d57c224ebebca095f7cf15a397311c30cd5
Alex Ly <alexly@Alexs-MBP.home>
2016-03-21 00:03:40 -0400

    Able to console log request params on front and back end, time to make register work on DB.

--- controllers/router.js
+++ controllers/router.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var router = express.Router();
 
+// Models
+var Users = require('../models/users.js');
+
 router.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+router.post('/register', function(req, res){
+  console.log(req.body);
 });
 
 module.exports = router;
