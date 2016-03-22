c4e9ce936d03f72e307ea13221ebd9ad1ccdacb8
Alex Ly <alexly@Alexs-MBP.home>
2016-03-21 23:41:24 -0400

    Passwords are now being encrypted.

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
 
 var usersSchema = new Schema({
   username:{
@@ -19,6 +20,23 @@ var usersSchema = new Schema({
   //   type: Schema.Types.ObjectID,
   //   ref:'Item'
   // }]
+});
+
+usersSchema.pre('save', function(next){
+  var user = this;
+
+  bcrypt.genSalt(10, function(err, salt){
+    if(err){
+      return next(err);
+    }else{
+      bcrypt.hash(user.password, salt, function(err, hash){
+        if(err){return next(err)}
+
+        user.password = hash;
+        next();
+      });
+    }
+  });
 });
 
 module.exports = mongoose.model('User', usersSchema);
